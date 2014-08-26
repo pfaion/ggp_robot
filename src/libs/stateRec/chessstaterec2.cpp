ba7fe867b9b0a2e4cc218d1790e6d2d71652ac97
pfaion <p.faion@web.de>
2014-08-26 18:29:09 +1000

    did something...

--- src/libs/stateRec/chessstaterec2.cpp
+++ src/libs/stateRec/chessstaterec2.cpp
@@ -59,23 +59,29 @@ void ChessStateRec2::setCamera(boost::shared_ptr<Camera>& cp) {
 bool ChessStateRec2::start() {
   PRINT("[SREC] Starting...");
   cam->listenToCloudStream(false);
-  cam->setCloudTopic("/camera/depth/points");
+  cam->setCloudTopic("/camera/depth_registered/points");
   cam->listenToCloudStream(true);
-  cam->listenToImageStream(false);
+  cam->listenToImageStream(true);
 
   KeyListener keyboard;
   keyboard.start();
 
+  typedef pcl::PointXYZRGB PointType;
+
   while(ros::ok()) {
+
+    viewer.removeAllPointClouds();
+    viewer.removeAllShapes();
+
     PRINT("[SREC] Fetching cloud.");
     pcl::PCLPointCloud2::ConstPtr cloud = cam->getPclCloud();
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pc(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<PointType>::Ptr pc(new pcl::PointCloud<PointType>());
     pcl::fromPCLPointCloud2(*cloud, *pc);
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered(new
-        pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<PointType>::Ptr filtered(new
+        pcl::PointCloud<PointType>());
 
-    pcl::CropBox<pcl::PointXYZ> crop;
+    pcl::CropBox<PointType> crop;
     std::vector<cv::Point3f> reg = board->getRotatedBoundingBox();
     cv::Point3f min = reg[0];
     cv::Point3f max = reg[0];
@@ -103,8 +109,8 @@ bool ChessStateRec2::start() {
     // PCL Cylinder Segmentation Tutorial:
 
     // Estimate point normals
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+    pcl::NormalEstimation<PointType, pcl::Normal> ne;
+    pcl::search::KdTree<PointType>::Ptr tree(new pcl::search::KdTree<PointType>());
     pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
     ne.setSearchMethod (tree);
     ne.setInputCloud (filtered);
@@ -112,7 +118,7 @@ bool ChessStateRec2::start() {
     ne.compute (*normals);
 
     // Create the segmentation object for the planar model and set all the parameters
-    pcl::SACSegmentationFromNormals<pcl::PointXYZ, pcl::Normal> seg;
+    pcl::SACSegmentationFromNormals<PointType, pcl::Normal> seg;
     seg.setOptimizeCoefficients (true);
     seg.setModelType (pcl::SACMODEL_NORMAL_PLANE);
     seg.setNormalDistanceWeight (0.1);
@@ -127,13 +133,17 @@ bool ChessStateRec2::start() {
     seg.segment (*inliers_plane, *coefficients_plane);
 
     // Extract the planar inliers from the input cloud
-    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    pcl::ExtractIndices<PointType> extract;
     extract.setInputCloud (filtered);
     extract.setIndices (inliers_plane);
     extract.setNegative (false);
+    pcl::visualization::PointCloudColorHandlerRGBField<PointType>
+      rgb(filtered);
+    viewer.addPointCloud<PointType>(filtered, rgb,
+        "planepts");
 
     // Remove the planar inliers, extract the rest
-    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered2(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<PointType>::Ptr filtered2(new pcl::PointCloud<PointType>());
     extract.setNegative (true);
     extract.filter (*filtered2);
     pcl::ExtractIndices<pcl::Normal> extract_normals;
@@ -150,7 +160,7 @@ bool ChessStateRec2::start() {
     seg.setNormalDistanceWeight (0.1);
     seg.setMaxIterations (10000);
     seg.setDistanceThreshold (0.05);
-    seg.setRadiusLimits (0.022, 0.027);
+    seg.setRadiusLimits (0, 0.1);
     seg.setInputCloud (filtered2);
     seg.setInputNormals (normals2);
     // Obtain the cylinder inliers and coefficients
@@ -164,26 +174,26 @@ bool ChessStateRec2::start() {
     extract.setInputCloud (filtered2);
     extract.setIndices (inliers_cylinder);
     extract.setNegative (false);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cylinder (new
-        pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PointCloud<PointType>::Ptr cloud_cylinder (new
+        pcl::PointCloud<PointType> ());
     extract.filter (*cloud_cylinder);
 
 
-    viewer.removeAllPointClouds();
-    viewer.removeAllShapes();
 
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
+
+
+    pcl::visualization::PointCloudColorHandlerCustom<PointType>
       lightRed(cloud_cylinder, 255,130,130);
-    viewer.addPointCloud<pcl::PointXYZ>(cloud_cylinder, lightRed,
+    viewer.addPointCloud<PointType>(cloud_cylinder, lightRed,
         "cylinderpts");
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
+    pcl::visualization::PointCloudColorHandlerCustom<PointType>
       red(cloud_cylinder, 255,0,0);
     //viewer.addCylinder(*coefficients_cylinder, "cylinder");
 
     extract.setNegative(true);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered3(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<PointType>::Ptr filtered3(new pcl::PointCloud<PointType>());
     extract.filter(*filtered3);
-    viewer.addPointCloud<pcl::PointXYZ>(filtered3, "rest");
+    viewer.addPointCloud<PointType>(filtered3, "rest");
 
 
     // test: compute intersection between ransac plane and cylinder
@@ -202,19 +212,102 @@ bool ChessStateRec2::start() {
     Eigen::Vector4f b;
     b << -p, ox, oy, oz;
     Eigen::Vector4f x = M.partialPivLu().solve(b);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr intersect(new
-        pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointXYZ pt;
+    pcl::PointCloud<PointType>::Ptr intersect(new
+        pcl::PointCloud<PointType>);
+    PointType pt;
     pt.x = x(0);
     pt.y = x(1);
     pt.z = x(2);
     intersect->push_back(pt);
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
+    pcl::visualization::PointCloudColorHandlerCustom<PointType>
       green(intersect, 0,255,0);
-    viewer.addPointCloud<pcl::PointXYZ>(intersect, green, "intersect");
+    viewer.addPointCloud<PointType>(intersect, green, "intersect");
     viewer.setPointCloudRenderingProperties
       (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "intersect");
 
+
+    if(cloud_cylinder->size() != 0) {
+      Eigen::Vector3f vec(0,0,0);
+      // re-adjust basepoint from known radius
+      for(pcl::PointCloud<PointType>::iterator it = cloud_cylinder->begin();
+          it != cloud_cylinder->end();
+          ++it) {
+        // distance between base point pt and the projection of cloud-point (it)
+        // onto the cylinder axis (scaled relative to normal-vector-length)
+        // notice: the normal vector of the plane is the axis-vector of the
+        // cylinder
+        float factor = ((it->x - pt.x)*nx + (it->y - pt.y)*ny + (it->z -
+              pt.z)*nz);
+        // sum the vectors from the cloud-point to their projection-point on the
+        // cylinder axis 
+        vec(0) += (pt.x + factor*nx) - it->x;
+        vec(1) += (pt.y + factor*ny) - it->y;
+        vec(2) += (pt.z + factor*nz) - it->z;
+      }
+      // unit length
+      vec = 1/std::sqrt(vec(0)*vec(0) + vec(1)*vec(1) + vec(2)*vec(2)) * vec;
+
+      // re-scaling with known radius
+      float radius = 0.025;
+      float radius_scaling = radius - coefficients_cylinder->values[6];
+      Eigen::Vector3f newBase_raw = Eigen::Vector3f(pt.x, pt.y, pt.z) + radius_scaling * vec;
+
+      // now project new base onto plane
+      float distanceToPlane = nx*newBase_raw(0) + ny*newBase_raw(1) +
+        nz*newBase_raw(2) + p;
+      Eigen::Vector3f newBase = newBase_raw - distanceToPlane * Eigen::Vector3f(nx,ny,nz);
+
+      PRINT(Eigen::Vector3f(nx, ny, nz));
+      // show me your basepoint!
+      pcl::PointCloud<PointType>::Ptr base(new
+          pcl::PointCloud<PointType>);
+      PointType pt2;
+      pt2.x = newBase(0);
+      pt2.y = newBase(1);
+      pt2.z = newBase(2);
+      base->push_back(pt2);
+      pcl::visualization::PointCloudColorHandlerCustom<PointType>
+        basecol(base, 200,0,0);
+      viewer.addPointCloud<PointType>(base, basecol, "base");
+    viewer.setPointCloudRenderingProperties
+        (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "base");
+
+      pcl::PointCloud<PointType>::Ptr line(new
+          pcl::PointCloud<PointType>);
+      pcl::PointCloud<PointType>::Ptr line2(new
+          pcl::PointCloud<PointType>);
+      for(float i=0; i<150; i++) {
+        Eigen::Vector3f tmp = newBase + i/1000 * Eigen::Vector3f(nx, ny, nz);
+        PointType tmpp;
+        tmpp.x = tmp(0);
+        tmpp.y = tmp(1);
+        tmpp.z = tmp(2);
+        line->push_back(tmpp);
+        Eigen::Vector3f tmp2 = newBase + i/1000 * Eigen::Vector3f(vx, vy, vz);
+        PointType tmpp2;
+        tmpp2.x = tmp2(0);
+        tmpp2.y = tmp2(1);
+        tmpp2.z = tmp2(2);
+        line2->push_back(tmpp2);
+        Eigen::Vector3f tmp3 = newBase - i/1000 * Eigen::Vector3f(vx, vy, vz);
+        PointType tmpp3;
+        tmpp3.x = tmp3(0);
+        tmpp3.y = tmp3(1);
+        tmpp3.z = tmp3(2);
+        line2->push_back(tmpp3);
+      }
+      pcl::visualization::PointCloudColorHandlerCustom<PointType>
+        tmpcol(line, 200,0,200);
+      viewer.addPointCloud<PointType>(line, tmpcol, "tmp");
+    viewer.setPointCloudRenderingProperties
+        (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "tmp");
+      pcl::visualization::PointCloudColorHandlerCustom<PointType>
+        tmpcol2(line2, 0,200,200);
+      viewer.addPointCloud<PointType>(line2, tmpcol2, "tmp2");
+    viewer.setPointCloudRenderingProperties
+        (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "tmp2");
+
+    }
 
 
 
@@ -223,7 +316,13 @@ bool ChessStateRec2::start() {
     char c = keyboard.getChar();
     if(c == 'q') return true;
     if(c == 'r') return false;
-    if(c == 's') viewer.spin();
+    if(c == 's') {
+      while(ros::ok()) {
+        viewer.spinOnce(1);
+        if(keyboard.getChar() == 's') break;
+      }
+    }
+
 
   }
 }
